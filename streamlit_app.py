035d008875d3393cfbe0f9642fa5772c7dd700db
Sabrina20-git <sarkoun@consilia-data.com>
2024-11-29 12:16:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,24 +10,19 @@ st.write(
     """
 )
 
-
 name_on_order = st.text_input('Name on smoothie: ')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
-
-
-ingredients_list = st.multiselect(
-    'choose up to 5 Ingredients:'
-    ,my_dataframe
-    ,max_selections=5
-)
- 
 
 cnx=st.connection("snowflake")
 session = cnx.session()
+
+my_dataframe = session.table('smooothies.public.fruit_option').select(col('FRUIT_NAME'),col('SEARCH_ON'))
+
+#Convert the snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
 
 if ingredients_list:
    ingredients_string=''
@@ -54,10 +49,7 @@ if ingredients_list:
 
 
 import requests
-#Convert the snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
-pd_df=my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
+
 
 ingredients_list = st.multiselect(
     'choose up to 5 ingredients:'
